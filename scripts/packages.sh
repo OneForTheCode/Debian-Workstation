41b50013e9fb2e8eb2d83a40dad54049d0d6bfc4
Dennis de Houx <dennis@aio.be>
2022-04-22 05:02:01 +0200

    Add production packages

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -22,7 +22,7 @@ sudo apt install feh nitrogen thunar thunar-archive-plugin thunar-font-manager t
 sudo apt install chromium -y
 
 ### INSTALL PRODUCTION PACKAGES
-sudo apt install eog totem p7zip-full flameshot codium filezilla gimp inkscape qalculate -y
+sudo apt install eog totem p7zip-full flameshot codium filezilla gimp inkscape qalculate okular okular-backend-odp okular-backend-odt -y
 
 ### SET GRAPHICAL INTERFACE FIRST
 sudo systemctl enable plymouth
