12727342c6ab05653b6c510fda4f29327ec257cf
Dennis de Houx <dennis@aio.be>
2022-04-23 21:41:31 +0200

    Update and nicify the setup script

--- setup.sh
+++ setup.sh
@@ -1,32 +1,101 @@
 #!/bin/bash
 
-### UPDATE APT PACKAGES REPOSITORIES
-sudo apt update -y
+###############################################################################
+###                                                                         ###
+### One For The Code - Debian Workstion Migration Tool                      ###
+### ==================================================                      ###
+###                                                                         ###
+### Install script to setup debian minimal install to a debian workstation  ###
+### with bspwm (window manager) and lots of base packages and production    ###
+### tools.                                                                  ###
+###                                                                         ###
+###                                                                         ###
+### Version  : 1.0.0                                                        ###
+### Author   : Dennis de houx <dennis@aio.be>                               ###
+### Copyright: 2022 (c) One For The Code                                    ###
+###            2022 (c) All In One                                          ###
+### Links    : https://github.com/OneForTheCode/Debian-Workstation          ###
+###                                                                         ###
+###############################################################################
 
-### INSTALL GIT SO WE CAN CLONE THE PROJECT
-sudo apt install git -y
+
+
+### SHOW THE HEADER
+echo " "
+echo "###############################################################################"
+echo "###                                                                         ###"
+echo "### One For The Code - Debian Workstion Migration Tool                      ###"
+echo "###                                                                         ###"
+echo "###############################################################################"
+echo " "
+
+
+### CHECK IF SUDO IS INSTALLED
+read sudoCheck <<< "$(which sudo 2> /dev/null)"
+if [ -z "${sudoCheck}" ]; then
+    echo " !!! Migration failed !!!"
+    echo " "
+    echo " Please install and setup 'sudo' as this is prerequired to finish the migration"
+    echo " "
+    exit 1
+fi
+
+
+### CHECK IF PRE-REQUIRED PACKAGES ARE INSTALLED
+declare -a REQUIRED_PKGS=(dialog git)
+PACKAGES=""
+for PKG in "${REQUIRED_PKGS[@]}"; do
+    read Check <<< "$(which ${PKG} 2> /dev/null)"
+    if [ -z "${Check}" ]; then
+        PACKAGES="${PACKAGES} ${PKG}"
+    fi
+done
+if [ ! -z "${PACKAGES}" ]; then
+    echo -n " * Installing pre required packages, please wait..."
+    sudo apt -y -qq install ${PACKAGES} > /dev/null 2>&1
+    echo " Done."
+fi
+
 
 ### SETUP TEMP DIRECTORY
+echo -n " * Creating temporary directory for install scripts, please wait..."
 cd
 mkdir debian-temp-install-scripts
+echo " Done."
+echo -n " * Changing to temporary directory, please wait..."
 cd debian-temp-install-scripts
+echo " Done."
+
 
 ### CLONE THE PROJECT
+echo -n " * Downloading install scripts and files, please wait..."
 git clone https://github.com/OneForTheCode/Debian-Workstation.git
+echo " Done."
+
 
 ### LAUCH MIGRATION SCRIPT
+echo -n " * Starting the migration scripts, please wait..."
 cd Debian-Workstation/scripts
 chmod +x migration.sh
 ./migration.sh
+echo " Done."
+
 
 ### REMOVE TEMP DIRECTORY
+echo -n " * Removing temporary install scripts and files, please wait..."
 cd
 rm -rf debian-temp-install-scripts
+echo " Done."
+
 
 ### REBOOT IF MIGRATION FINISHED
 if [ -f "MIGRATION.reboot" ]; then
     rm -f MIGRATION.reboot
+    rm -f $0
     sudo reboot
 else
-    echo "Migration failed for some reason and system can't be automatic rebooted!!!"
+    echo " !!! Migration failed !!!"
+    echo " "
+    echo " System is not able to reboot, please reboot manually or restart the migration script."
+    echo " "
 fi
