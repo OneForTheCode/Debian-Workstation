e50e02cbe9a1dda7139cf49e6039b05871e67cc0
Dennis de Houx <dennis@aio.be>
2022-04-24 13:31:02 +0200

    Refactoring of migration script

--- scripts/migration.sh
+++ scripts/migration.sh
@@ -1,26 +1,76 @@
 #!/bin/bash
 
-### LOAD ENV CONFIG
-source env.sh
+###############################################################################
+###                                                                         ###
+### One For The Code - Debian Workstion Migration Tool                      ###
+### ==================================================                      ###
+###                                                                         ###
+### Install script to setup debian minimal install to a debian workstation  ###
+### with bspwm (window manager) and lots of base packages and production    ###
+### tools.                                                                  ###
+###                                                                         ###
+###                                                                         ###
+### Version  : 1.0.0                                                        ###
+### Author   : Dennis de houx <dennis@aio.be>                               ###
+### Copyright: 2022 (c) One For The Code                                    ###
+###            2022 (c) All In One                                          ###
+### Links    : https://github.com/OneForTheCode/Debian-Workstation          ###
+###                                                                         ###
+###############################################################################
+
+
+
+### LOAD CONFIGURATION FILE
+while read configLine; do
+    if [[ "${configLine}" != *"#"* ]] && [[ ! -z "${configLine}" ]]; then
+        if [[ "${configLine}" == *"=("* ]]; then
+            declare -a "${configLine}"
+        else
+            declare "${configLine}"
+        fi
+    fi
+done < migration.conf
+HOME_DIR="${HOME}"
+TEMP_DIR="${HOME}/debian-temp-install-scripts/Debian-Workstion"
+
+
+### LOAD SCRIPT FUNCTIONS
+source functions/dialog.fn
+source functions/repositories.fn
+source functions/packages.fn
+source functions/sysconfig.fn
+source functions/fonts.fn
+source functions/directories.fn
+source functions/dotconfig.fn
+
+
+### SHOW WELCOME MESSAGE
+dialog_welcome
+
 
 ### RUN REPOSITORIES SCRIPT
-./repositories.sh
+repositories_run
+
 
 ### RUN PACKAGE SCRIPT
-./packages.sh
+packages_run
+
 
 ### RUN SYSTEM CONFIG SCRIPT
-./sysconfig.sh
+sysconfig_run
+
 
 ### RUN FONTS SCRIPT
-./fonts.sh
+fonts_run
+
 
 ### RUN USER DIRECTORIES SCRIPT
-./directories.sh
+directories_run
+
 
 ### RUN DOT CONFIG SCRIPT
-./dotconfig.sh
+dotconfig_run
+
 
 ### CREATE REBOOT FILE
-cd
-touch MIGRATION.reboot
+touch ~/MIGRATION.reboot
